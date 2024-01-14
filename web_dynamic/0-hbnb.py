8fbfcf056f19ce1b8faa3be4c29ad03537afd8a3
irungundigirigi <irungundigirigi@gmail.com>
2024-01-14 20:43:11 +0300

    web dynamic update

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -22,7 +22,7 @@ def close_db(error):
 @app.route('/0-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    states = storage.all(State).values()
+    states = storage.all("State").values()
     states = sorted(states, key=lambda k: k.name)
     cache_id = uuid.uuid4();
     st_ct = []
@@ -36,7 +36,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
